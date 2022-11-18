50cefcf510348489bb2125e2b52b2bfef08df693
wellá <info@lost.design>
2022-11-18 16:47:51 +0100

    feat: init docs repo
    Signed-off-by: wellá <info@lost.design>

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: 'bos.wiki',
+    description: 'Offizielle Dokumentation',
+    image: 'https://cdn.discordapp.com/emojis/1003398170962833438.webp?size=240&quality=lossless',
+    socials: {
+      twitter: 'boswiki',
+      github: 'boswiki',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    footer: {
+      credits: {
+        text: ''
+      },
+      iconLinks: [
+        {
+          href: 'https://bos.wiki',
+          icon: 'ph:house-simple-fill'
+        }
+      ]
+    }
+  }
+})
